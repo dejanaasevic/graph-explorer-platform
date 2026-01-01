cf538550ccab94036dd29363c39e201d923f21bb
dejana šević <dejana.sevic24@gmail.com>
2026-01-01 16:40:42 +0100

    feat: add Edge class

--- api/src/api/model/graph.py
+++ api/src/api/model/graph.py
@@ -9,3 +9,13 @@ class Node:
 
     def __repr__(self):
         return f"Node(id={self.node_id}, attributes={self.attributes})"
+
+class Edge:
+    def __init__(self, source : Node, target : Node, attributes: Dict[str, Any] = None):
+        self.id : uuid.UUID = uuid.uuid4()
+        self.source : Node = source
+        self.target : Node = target
+        self.attributes: Dict[str, Any] = attributes or {}
+
+    def __repr__(self):
+        return f"Edge({self.source.node_id} --> {self.target.node_id})"
