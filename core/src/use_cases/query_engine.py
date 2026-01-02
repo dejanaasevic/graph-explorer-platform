2b398e59ebc2e155905cf489923fa37687acfdf4
dejana šević <dejana.sevic24@gmail.com>
2026-01-02 21:01:17 +0100

    #6 feat: add QueryEngine skeleton

---
+++ core/src/use_cases/query_engine.py
@@ -0,0 +1,13 @@
+from api.src.api.model import Graph
+
+
+class QueryEngine:
+
+    @staticmethod
+    def search(graph: Graph, text: str) -> Graph:
+        pass
+
+    @staticmethod
+    def filter(graph: Graph, expression: str) -> Graph:
+        pass
+
