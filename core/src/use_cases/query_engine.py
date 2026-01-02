beda30dee77791cebaf345b9b5f2d9eae4762793
dejana šević <dejana.sevic24@gmail.com>
2026-01-02 21:14:13 +0100

    #6 feat: add filter functionality for graph structure

--- core/src/use_cases/query_engine.py
+++ core/src/use_cases/query_engine.py
@@ -3,6 +3,7 @@ from typing import List, Dict
 
 from api.src.api.model import Graph
 from api.src.api.model.graph import Node
+from core.src.use_cases.query_parser import FilterExpression, Parser
 
 
 class QueryEngine:
@@ -20,7 +21,15 @@ class QueryEngine:
 
     @staticmethod
     def filter(graph: Graph, expression: str) -> Graph:
-        pass
+        filter_expression: FilterExpression = Parser.parse(expression)
+        valid_nodes: List[Node] = []
+        for node in graph.get_nodes():
+            if node.attributes.get(filter_expression.attribute.lower()) is None:
+                continue
+            if QueryEngine.is_valid_node(node, filter_expression):
+                valid_nodes.append(node.copy(True))
+        new_graph: Graph = QueryEngine.build_subgraph(valid_nodes, graph)
+        return new_graph
 
     @staticmethod
     def build_subgraph(nodes: List[Node], graph: Graph) -> Graph:
@@ -34,3 +43,30 @@ class QueryEngine:
             if edge.source.id in map_nodes and edge.target.id in map_nodes:
                 new_graph.add_edge(edge.copy(map_nodes[edge.source.id], map_nodes[edge.target.id]))
         return new_graph
+
+    @staticmethod
+    def is_valid_node(node: Node, filter_expression: FilterExpression) -> bool:
+        attribute_value = node.attributes.get(filter_expression.attribute.lower())
+
+        if attribute_value is None:
+            return False
+
+        if not (isinstance(attribute_value, type(filter_expression.value)) or
+                (isinstance(attribute_value, (int, float)) and isinstance(filter_expression.value, (int, float)))):
+            raise Exception(
+                f"error: Types do not match! Attribute: {type(attribute_value)}, filter value: {type(filter_expression.value)}")
+
+        match filter_expression.operator:
+            case "==":
+                return attribute_value == filter_expression.value
+            case ">":
+                return attribute_value > filter_expression.value
+            case ">=":
+                return attribute_value >= filter_expression.value
+            case "<":
+                return attribute_value < filter_expression.value
+            case "<=":
+                return attribute_value <= filter_expression.value
+            case "!=":
+                return attribute_value != filter_expression.value
+            case _: raise Exception(f"error: operator {filter_expression.operator} is not supported!")
