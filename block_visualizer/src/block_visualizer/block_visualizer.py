8d845126cac71b55ff3d2dd991a37dbeaf108cf9
niksa <cvorovicniksa@gmail.com>
2026-01-10 16:20:16 +0100

    #12 Block Visualizer package base implementation

---
+++ block_visualizer/src/block_visualizer/block_visualizer.py
@@ -0,0 +1,21 @@
+from api.model import Graph
+from api.plugins import VisualizerPlugin
+
+class BlockVisualizer(VisualizerPlugin):
+    def name(self) -> str:
+        return "Block Visualizer"
+
+    def identifier(self) -> str:
+        return "block_visualizer"
+
+    def render(self, graph:Graph, **kwargs) -> str:
+        graph_str = ""
+        for node in graph.get_nodes():
+            graph_str += f"id: {node.id}\n"
+            for key in node.attributes:
+                graph_str += f"\t{key}: {node.attributes[key]}\n"
+        for edge in graph.get_edges():
+            graph_str += f"\t{edge.id}\n"
+            for key in edge.attributes:
+                graph_str += f"\t{key}: {edge.attributes[key]}\n"
+        return graph_str
