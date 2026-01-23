b49be24155ba53ae4cb77cea02e2a7ab7eaa60a1
dejana šević <dejana.sevic24@gmail.com>
2026-01-23 16:06:11 +0100

    #14 feat: add graph loader and file handling logic

--- json_data_source/src/json_data_source/json_data_source.py
+++ json_data_source/src/json_data_source/json_data_source.py
@@ -1,3 +1,6 @@
+import json
+from typing import Dict, Any
+
 from api.model import *
 from api.plugins import DataSourcePlugin
 
@@ -9,4 +12,22 @@ class JsonDataSource(DataSourcePlugin):
         return "json_data_source"
 
     def load(self, **kwargs) -> Graph:
+        file_path : str = kwargs.get("file_path")
+        if file_path is None:
+            raise AttributeError("file_path is required")
+
+        directed : bool = kwargs.get("directed", True)
+
+        graph : Graph = Graph(directed=directed)
+        id_to_node : Dict[str, Node] = {}
+
+        with open(file_path) as file:
+            data = json.load(file)
+
+        self.parse_json(data, graph, id_to_node)
+
+        return graph
+
+    def parse_json(self, data : Any, graph : Graph, id_to_node : Dict[str, Node],
+                   parent_node : Node = None, relation : str = None):
         pass
