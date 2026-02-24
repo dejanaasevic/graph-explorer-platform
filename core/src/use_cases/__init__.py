812ab01aa1f4aba02d8e896ab8d0d76871573a85
niksa <cvorovicniksa@gmail.com>
2026-02-24 13:09:13 +0100

    #22 Django Base HTML Template

---
+++ core/src/use_cases/__init__.py
@@ -0,0 +1,3 @@
+from .plugin_recognition import PluginService
+
+__all__ = ['PluginService']
--- graph_explorer/app/views.py
+++ graph_explorer/app/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from use_cases import PluginService
+from api.model import Graph
+from importlib.resources import files
 
 def index(request):
-    return HttpResponse("Graph Explorer is working!")
+    plugin_service = PluginService()
+    plugin_service.load_plugins("graph.data_source")
+    plugin_service.load_plugins("graph.visualizer")
+    graph: Graph = plugin_service.plugins["graph.data_source"][0].load(file_path=files('data')
+                                                                       .joinpath('game_studio_data.json'))
+    graph_vis = plugin_service.plugins["graph.visualizer"][0].render(graph)
+    return render(request,'index.html', {"graph":graph_vis})
