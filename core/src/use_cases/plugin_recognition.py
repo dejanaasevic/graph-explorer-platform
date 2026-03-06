c2ae4cb62bfabef2a633c48516fbb87eca64e92c
niksa <cvorovicniksa@gmail.com>
2026-03-06 23:44:44 +0100

    #49 Comments for Block Visualizer and Plugin Service and code cleanup

--- core/src/use_cases/plugin_recognition.py
+++ core/src/use_cases/plugin_recognition.py
@@ -4,11 +4,16 @@ from typing import List
 from api.plugins import Plugin
 
 class PluginService(object):
-
+    """
+    Detects and loads installed plugins.
+    """
     def __init__(self):
         self.plugins: dict[str,List[Plugin]] = {}
 
     def load_plugins(self, group: str):
+        """
+        Loads plugins from a given group.
+        """
         self.plugins[group] = []
         for ep in entry_points(group=group):
             p = ep.load()
